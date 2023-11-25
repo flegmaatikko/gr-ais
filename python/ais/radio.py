29963f20b655c5c06e37538dce0fe49d88c25556
flegmaatikko <flegmaatikko@users.noreply.github.com>
2023-11-25 18:00:39 +0200

    Move from OptionParser to ArgumentParser

--- python/ais/radio.py
+++ python/ais/radio.py
@@ -23,12 +23,12 @@
 # You pass it options, it gives you data.
 # It uses the pubsub interface to allow clients to subscribe to its data feeds.
 
-from gnuradio import gr, gru, eng_notation, filter, blocks, digital
+from gnuradio import gr, filter, blocks, digital
 from gnuradio.filter import optfir
 from gnuradio.filter import pfb
 from gnuradio.eng_option import eng_option
 from gnuradio.gr.pubsub import pubsub
-from optparse import OptionParser, OptionGroup
+from argparse import ArgumentParser as OptionParser
 import threading
 import time
 import sys
@@ -99,30 +99,29 @@ class ais_radio (gr.top_block, pubsub):
 
   @staticmethod
   def add_radio_options(parser):
-    group = OptionGroup(parser, "Receiver setup options")
+    group = parser.add_argument_group("Receiver setup options")
 
     #Choose source
-    group.add_option("-s","--source", type="string", default="uhd",
-                      help="Choose source: uhd, osmocom, <filename>, or <ip:port> [default=%default]")
+    group.add_argument("-s","--source", type=str, default="uhd",
+                      help="Choose source: uhd, osmocom, <filename>, or <ip:port> [default=%(default)s]")
 
     #UHD/Osmocom args
-    group.add_option("-R", "--subdev", type="string",
+    group.add_argument("-R", "--subdev", type=str,
                       help="select USRP Rx side A or B", metavar="SUBDEV")
-    group.add_option("-A", "--antenna", type="string",
+    group.add_argument("-A", "--antenna", type=str,
                       help="select which antenna to use on daughterboard")
-    group.add_option("-D", "--args", type="string",
+    group.add_argument("-D", "--args", type=str,
                       help="arguments to pass to radio constructor", default="")
-    group.add_option("-g", "--gain", type="int", default=None,
+    group.add_argument("-g", "--gain", type=int, default=None,
                       help="set RF gain", metavar="dB")
-    parser.add_option("-e", "--error", type="eng_float", default=0,
-                        help="set offset error of device in PPM [default=%default]")
+    parser.add_argument("-e", "--error", type=float, default=0,
+                        help="set offset error of device in PPM [default=%(default)s]")
     #RX path args
-    group.add_option("-r", "--rate", type="eng_float", default=250e3,
-                      help="set sample rate [default=%default]")
-    group.add_option("-S", "--singlechannel", action="store_true", default=False,
-                     help="Use only a single channel instead of looking at both A & B [default=%default]")
-
-    parser.add_option_group(group)
+    group.add_argument("-r", "--rate", type=float, default=250e3,
+                      help="set sample rate [default=%(default)s]")
+    group.add_argument("-S", "--singlechannel", action="store_true", default=False,
+                     help="Use only a single channel instead of looking at both A & B [default=%(default)s]")
+    #parser.add_argument_group(group)
 
   def live_source(self):
     return self._options.source=="uhd" or self._options.source=="osmocom"
@@ -146,7 +145,7 @@ class ais_radio (gr.top_block, pubsub):
     return self._u.get_gain() if self.live_source() else 0
 
   def get_rate(self):
-    return self._u.get_samp_rate() if self.live_source() else self._rate
+    return self._u.get_sample_rate() if self.live_source() else self._rate
 
   def _setup_source(self, options):
     if options.source == "uhd":
@@ -185,9 +184,9 @@ class ais_radio (gr.top_block, pubsub):
     #and set up accordingly.
     elif options.source == "osmocom": #RTLSDR dongle or HackRF Jawbreaker
         import osmosdr
-        src = osmosdr.source(options.args)
+        src = osmosdr.osmosdr_python.source(options.args)
         src.set_sample_rate(options.rate)
-        src.get_samp_rate = src.get_sample_rate #alias for UHD compatibility
+        #src.get_samp_rate = src.get_sample_rate #alias for UHD compatibility
         if not src.set_center_freq(162.0e6 * (1 + options.error/1.e6)):
             print("Failed to set initial frequency")
         else:
